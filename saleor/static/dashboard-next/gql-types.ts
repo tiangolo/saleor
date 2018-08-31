517d523521078ac985cb0555db3903bd8d0feb69
dominik-zeglen <flesz3@o2.pl>
2018-08-31 22:06:17 +0200

    Add menu item views

--- saleor/static/dashboard-next/gql-types.ts
+++ saleor/static/dashboard-next/gql-types.ts
@@ -353,7 +353,7 @@ export interface MenuDetailsQuery {
   } | null,
 };
 
-export interface menuItemQueryVariables {
+export interface MenuItemDetailsQueryVariables {
   id: string,
   first?: number | null,
   after?: string | null,
@@ -361,11 +361,12 @@ export interface menuItemQueryVariables {
   before?: string | null,
 };
 
-export interface menuItemQuery {
+export interface MenuItemDetailsQuery {
   // Lookup a menu item by ID.
   menuItem:  {
     // The ID of the object.
     id: string,
+    name: string,
     parent:  {
       // The ID of the object.
       id: string,
@@ -374,6 +375,7 @@ export interface menuItemQuery {
     menu:  {
       // The ID of the object.
       id: string,
+      name: string,
     },
     // URL to the menu item.
     url: string | null,
@@ -398,6 +400,7 @@ export interface menuItemQuery {
         node:  {
           // The ID of the object.
           id: string,
+          name: string,
           // URL to the menu item.
           url: string | null,
           category:  {
@@ -415,8 +418,22 @@ export interface menuItemQuery {
             id: string,
             name: string,
           } | null,
+          children:  {
+            // A total count of items in the collection
+            totalCount: number | null,
+          } | null,
         },
       } >,
+      pageInfo:  {
+        // When paginating backwards, are there more items?
+        hasPreviousPage: boolean,
+        // When paginating forwards, are there more items?
+        hasNextPage: boolean,
+        // When paginating backwards, the cursor to continue.
+        startCursor: string | null,
+        // When paginating forwards, the cursor to continue.
+        endCursor: string | null,
+      },
     } | null,
   } | null,
 };
